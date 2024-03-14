6a86a6b36c463988648ad22d76bfe8d3b84dc698
Ashish Gaur <ashishgaurp91@gmail.com>
2024-03-14 12:22:58 +0530

    Setup omniauth

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,24 @@
+class OmniauthCallbacksController < ApplicationController
+
+  def twitter
+    return render plain: "Successful"
+    Rails.logger.info(auth)
+
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).
+      first_or_initialize(
+        name: auth.info.name,
+        username: auth.info.nickname,
+        image: auth.info.image,
+        token: auth.credentials.token,
+        secret: auth.credentials.secret,
+      )
+    twitter_account.save!
+
+    redirect_to root_path, notice: "Successfully connected your account"
+  end
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
+end
